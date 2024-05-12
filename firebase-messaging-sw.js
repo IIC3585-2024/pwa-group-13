7c5c811e20ee10cb0118ef4ed303ccdbeb36f366
Yuwen Cheng <ycheng@uc.cl>
2024-05-12 13:08:42 -0400

    chore: Remove unused Firebase code and update dependencies

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,127 +1,5 @@
-const staticAssets = [
-  './',
-  './index.html',
-  './styles.css',
-  './app.js',
-  './spliti.js',
-  './db.js',
-  './firebase-messaging-sw.js',
-  './serviceworker.js',
-];
-
-self.addEventListener('install', async event => {
-  event.waitUntil(
-    caches.open('static-pizza').then(cache => {
-      return cache.addAll(staticAssets);
-    })
-  );
-});
-
-self.addEventListener('fetch', event => {
-  console.log('Handling fetch event for', event.request.url);
-
-  if (!event.request.url.startsWith('http')) {
-    console.log('Request not handled because it is not HTTP:', event.request.url);
-    return; // No manejar solicitudes de otros esquemas
-  }
-  
-  event.respondWith(
-    caches.match(event.request)
-      .then(function(response) {
-        if (response) {
-          console.log('Found response in cache:', response);
-          return response;
-        }
-
-        console.log('No response found in cache. About to fetch from network...');
-        return fetch(event.request).then(
-          function(response) {
-            console.log('Response from network is:', response);
-
-            if(!response || response.status !== 200 || response.type !== 'basic') {
-              return response;
-            }
-
-            console.log('Caching the response to', event.request.url);
-            var responseToCache = response.clone();
-
-            caches.open('static-pizza')
-              .then(function(cache) {
-                cache.put(event.request, responseToCache);
-              });
-
-            return response;
-          }
-        );
-      })
-  );
-});
-
-self.addEventListener('activate', event => {
-  event.waitUntil(self.clients.claim());
-});
-
-// self.addEventListener('fetch', event => {
-//   const {request} = event;
-//   const url = new URL(request.url);
-//   if(url.origin === location.origin) {
-//       event.respondWith(cacheData(request));
-//   } else {
-//       event.respondWith(networkFirst(request));
-//   }
-
-// });
-
-async function cacheData(request) {
-  const cachedResponse = await caches.match(request);
-  return cachedResponse || fetch(request);
-}
-
-async function networkFirst(request) {
-  if (!['http:', 'https:', 'ftp:'].includes(new URL(request.url).protocol)) {
-    return fetch(request);
-  }
-  const cache = await caches.open('dynamic-pizza');
-
-  try {
-      const response = await fetch(request);
-      cache.put(request, response.clone());
-      return response;
-  } catch (error){
-      return await cache.match(request);
-
-  }
-
-
-
-}
-
-
-importScripts('https://www.gstatic.com/firebasejs/8.10.1/firebase-app.js');
-importScripts('https://www.gstatic.com/firebasejs/8.10.1/firebase-messaging.js');
-
-function openDB() {
-  return new Promise((resolve, reject) => {
-    const request = indexedDB.open("SplitDB", 1);
-    request.onupgradeneeded = function() {
-      const db = request.result;
-      if (!db.objectStoreNames.contains("tokens")) {
-        db.createObjectStore("tokens", { keyPath: "id" });
-      }
-    };
-    request.onsuccess = () => resolve(request.result);
-    request.onerror = () => reject(request.error);
-  });
-}
-
-async function saveToken(token) {
-  const db = await openDB();
-  const tx = db.transaction("tokens", "readwrite");
-  const store = tx.objectStore("tokens");
-  await store.put({ id: "userToken", token: token });
-  return tx.complete;
-}
-
+importScripts('https://www.gstatic.com/firebasejs/9.0.0/firebase-app-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/9.0.0/firebase-messaging-compat.js');
 
 const firebaseConfig = {
   apiKey: "AIzaSyCO-NBLLqUwoaZvevPtEi9NDInVaZfuJjM",
@@ -131,23 +9,19 @@ const firebaseConfig = {
   messagingSenderId: "230226191544",
   appId: "1:230226191544:web:95783b4e7c268e8c53804d"
 };
-firebase.initializeApp(firebaseConfig)
+
+firebase.initializeApp(firebaseConfig);
+
 const messaging = firebase.messaging();
 
-//console.log(messaging.getToken())
-messaging.getToken().then(async (token)=> {
-  console.log(token);
-  await saveToken(token);
-})
 
-messaging.setBackgroundMessageHandler(function(payload) {
-  // const title = payload.notification.title;
-  const title = 'Notificación firebase-messaging-sw.js';
-  const options = {
-    body: payload.notification.body,
-    icon: payload.notification.image
-  };
-  console.log('Mostrando notificación:', title, options);
-  // new Notification(title, options);
-});
+// messaging.onBackgroundMessage(function(payload) {
+//   console.log('Recibido en background', payload);
+//   const notificationTitle = payload.notification.title;
+//   const notificationOptions = {
+//       body: payload.notification.body,
+//       icon: './images/pizza.png'
+//   };
 
+//   return self.registration.showNotification(notificationTitle, notificationOptions);
+// });
--- serviceworker.js
+++ serviceworker.js
@@ -6,7 +6,8 @@ const staticAssets = [
   './spliti.js',
   './db.js',
   './firebase-messaging-sw.js',
-  './serviceworker.js',
+  './firebase-init.js',
+  './images/pizza.png',
   // Íconos Android comunes
   './images/icons/android/android-launchericon-192-192.png',
   './images/icons/android/android-launchericon-512-512.png',
@@ -18,6 +19,7 @@ const staticAssets = [
   './images/icons/ios/512.png'
 ];
 
+
 self.addEventListener('install', async event => {
   event.waitUntil(
     caches.open('static-pizza').then(cache => {
@@ -27,36 +29,95 @@ self.addEventListener('install', async event => {
 });
 
 self.addEventListener('fetch', event => {
-  const {request} = event;
-  const url = new URL(request.url);
-  if(url.origin === location.origin) {
-      event.respondWith(cacheData(request));
-  } else {
-      event.respondWith(networkFirst(request));
-  }
+  console.log('Handling fetch event for', event.request.url);
 
+  if (!event.request.url.startsWith('http')) {
+    // console.log('Request not handled because it is not HTTP:', event.request.url);
+    return; // No manejar solicitudes de otros esquemas
+  }
+  
+  event.respondWith(
+    // Primero intentar por la red, si falla, intentar desde la caché
+    fetch(event.request)
+      .catch(() => {
+        console.log('Not found in network. Trying cache...')
+        return caches.match(event.request)
+      })
+    );
+  //   caches.match(event.request)
+  //     .then(function(response) {
+  //       if (response) {
+  //         console.log('Found response in cache:', response);
+  //         return response;
+  //       }
+
+  //       console.log('No response found in cache. About to fetch from network...');
+  //       return fetch(event.request).then(
+  //         function(response) {
+  //           console.log('Response from network is:', response);
+
+  //           if(!response || response.status !== 200 || response.type !== 'basic') {
+  //             return response;
+  //           }
+
+  //           console.log('Caching the response to', event.request.url);
+  //           var responseToCache = response.clone();
+
+  //           caches.open('static-pizza')
+  //             .then(function(cache) {
+  //               cache.put(event.request, responseToCache);
+  //             });
+
+  //           return response;
+  //         }
+  //       );
+  //     })
+  // );
 });
 
-async function cacheData(request) {
-  const cachedResponse = await caches.match(request);
-  return cachedResponse || fetch(request);
-}
+self.addEventListener('activate', event => {
+  event.waitUntil(self.clients.claim());
+});
 
-async function networkFirst(request) {
-  if (!['http:', 'https:', 'ftp:'].includes(new URL(request.url).protocol)) {
-    return fetch(request);
-  }
-  const cache = await caches.open('dynamic-pizza');
+// self.addEventListener('install', async event => {
+//   event.waitUntil(
+//     caches.open('static-pizza').then(cache => {
+//       return cache.addAll(staticAssets);
+//     })
+//   );
+// });
 
-  try {
-      const response = await fetch(request);
-      cache.put(request, response.clone());
-      return response;
-  } catch (error){
-      return await cache.match(request);
+// self.addEventListener('fetch', event => {
+//   const {request} = event;
+//   const url = new URL(request.url);
+//   if(url.origin === location.origin) {
+//       event.respondWith(cacheData(request));
+//   } else {
+//       event.respondWith(networkFirst(request));
+//   }
 
-  }
+// });
+
+// async function cacheData(request) {
+//   const cachedResponse = await caches.match(request);
+//   return cachedResponse || fetch(request);
+// }
+
+// async function networkFirst(request) {
+//   if (!['http:', 'https:', 'ftp:'].includes(new URL(request.url).protocol)) {
+//     return fetch(request);
+//   }
+//   const cache = await caches.open('dynamic-pizza');
+
+//   try {
+//       const response = await fetch(request);
+//       cache.put(request, response.clone());
+//       return response;
+//   } catch (error){
+//       return await cache.match(request);
+
+//   }
 
 
 
-}
+// }
